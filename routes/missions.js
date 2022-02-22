1d759c16d68aa31fa682892b3122c9214689768f
NashOwens <74252701+NashOwens@users.noreply.github.com>
2022-02-22 02:08:22 -0800

    Moved login from server to its route folder

--- routes/missions.js
+++ routes/missions.js
@@ -14,9 +14,6 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('missions.ejs')
 })
-router.get('/login', (req, res) => {
-    res.render('login.ejs')
-})
 
 router.get('/create', (req, res) => {
     res.render('createmission.ejs')
