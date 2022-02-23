a344c62d4d6017aa185983d8994c6b6b6d24a3ac
ethanp3 <78411793+ethanp3@users.noreply.github.com>
2022-02-23 13:33:17 +0000

    Configured Layout for Login page
    Layout page customized and now running its own stylesheet, with Bootstrap. Paths changed to make sure that the default layout not applying to this page

--- routes/login.js
+++ routes/login.js
@@ -5,66 +5,66 @@ const router = express.Router()
 const bodyparser = require("body-parser")
 const request = require("request")
 const res = require("express/lib/response")
-const url = require('url'); 
+const url = require('url');
 
-let urlencodedparser = bodyparser.urlencoded( { extended: false })
+let urlencodedparser = bodyparser.urlencoded({ extended: false })
 
 //router.get("/login", (res, req) => {
 //    req.render('login.ejs')
 //}) 
 
 router.get('/', (req, res) => {
-  //router.set('view engine', 'ejs')
-  res.render('login.ejs')
+    //router.set('view engine', 'ejs')
+    res.render('login', { layout: 'login-layout' })
 })
 
 function Authorization(data, res) {
-  // Create buffer object, specifying utf8 as encoding
-  let string = data.client_id+":"+data.client_secret
-  let bufferObj = Buffer.from(string, "utf8");
+    // Create buffer object, specifying utf8 as encoding
+    let string = data.client_id + ":" + data.client_secret
+    let bufferObj = Buffer.from(string, "utf8");
 
-  // Encode the Buffer as a base64 string
-  let base64String = bufferObj.toString("base64");
-  var options = {
-      'method': 'POST',
-      'url': 'https://hallam.sci-toolset.com/api/v1/token',
-      'headers': {
-        'Authorization': `Basic ${base64String}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      form: {
-        'username': data.username,
-        'password': data.password,
-        'scope': 'read write',
-        'client_id': data.client_id,
-        'client_secret': data.client_secret,
-        'grant_type': 'password'
-      }
+    // Encode the Buffer as a base64 string
+    let base64String = bufferObj.toString("base64");
+    var options = {
+        'method': 'POST',
+        'url': 'https://hallam.sci-toolset.com/api/v1/token',
+        'headers': {
+            'Authorization': `Basic ${base64String}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            'username': data.username,
+            'password': data.password,
+            'scope': 'read write',
+            'client_id': data.client_id,
+            'client_secret': data.client_secret,
+            'grant_type': 'password'
+        }
     };
 
-  request(options, function(error, response) {  
-      if (error) {
-        throw new Error(error);
-      } 
-      var data = JSON.parse(response.body)
-      res(data)
-  })
+    request(options, function(error, response) {
+        if (error) {
+            throw new Error(error);
+        }
+        var data = JSON.parse(response.body)
+        res(data)
+    })
 }
 
 
 router.post('/', urlencodedparser, (request, response) => {
-  //router.set('view engine', 'ejs')
-  Authorization(request.body, function(res) {
-    if(res.access_token!=null){
-      console.log("Response:");
-      //console.log(res);
-      response.redirect(url.format({ pathname:"/index", query: res, format: 'json' }))
-      
-    } else {
-      console.log(res);
-      response.redirect(url.format({ pathname:"/login", query: res, format: 'json' }))
-    }
-  })
+    //router.set('view engine', 'ejs')
+    Authorization(request.body, function(res) {
+        if (res.access_token != null) {
+            console.log("Response:");
+            //console.log(res);
+            response.redirect(url.format({ pathname: "/index", query: res, format: 'json' }))
+
+        } else {
+            console.log(res);
+            response.redirect(url.format({ pathname: "/login", query: res, format: 'json' }))
+        }
+    })
 })
 
 
