b6f0ae6af6883b7ed989881fbfe66af764dbf5a7
ethanp3 <78411793+ethanp3@users.noreply.github.com>
2022-02-25 12:45:43 +0000

    Added Map Zoom and Table Highlights
    Add Map Zoom feature on clicking a table row using the coordinates from the row

--- public/minimap.js
+++ public/minimap.js
@@ -1,37 +1,10 @@
-var addressPoints = [
-    [51.5, -0.5, "ID1"],
-    [52.5, -0.1, "ID2"],
-    [53.5, -1, "ID3"],
-    [51, -2, "ID4"],
-    [52.5, -2.5, "ID5"],
-    [51.5, -3, "ID6"],
-    [55, -2, "ID7"]
-]
-
-var map = L.map('minimap').setView([53, -4], 6);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var markers = L.markerClusterGroup();
-
-for (var i = 0; i < addressPoints.length; i++) {
-    var a = addressPoints[i];
-    var title = a[2];
-    var marker = L.marker(new L.LatLng(a[0], a[1]), {
-        title: title
-    });
-    marker.bindPopup(title);
-    markers.addLayer(marker);
-    map.addLayer(markers);
-}
-
-//Use this to create all single map pin
-//var marker = L.marker([51.5, -3]).addTo(map);
-//marker.bindPopup("<b>Scene</b><br>") //add data we want
-
 //Use this to create  missions containing scenes
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
 
 var mission = {
     "type": "FeatureCollection",
@@ -55,6 +28,16 @@ var mission = {
         }
     }]
 }
+
+var map = new L.Map('minimap', {
+    center: [53, -4],
+    zoom: 6,
+    cursor: true
+});
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 L.geoJSON(mission, {
     onEachFeature: onEachFeature
